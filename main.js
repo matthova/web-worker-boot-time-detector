251d7b119c5187de580d5caf1a85141918c77aa4
Matt Hova <matthova87@gmail.com>
2021-08-03 09:58:27 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const worker = new Worker('worker.js');
 
 worker.onmessage = function (e) {
-  let message = 'requestAnimationFrame is not supported by webworkers on this client';
+  let message = 'requestAnimationFrame is not supported by webworkers on this client. Try this code on Chrome.';
   if (e.data !== -1) {
     const lastBoot = new Date(Date.now() - e.data);
     message = `Device last boot: ${lastBoot.toTimeString()}`;
